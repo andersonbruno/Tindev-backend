0397a569e890f06f563c8762fd32ff02930f12bb
Anderson Santos <bruno-squall@hotmail.com>
2020-09-07 14:31:38 -0300

    added gitignore

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,11 @@ import express from 'express';
 import routes from './routes';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import db from './config/database.json';
 
 const server = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-87u19.mongodb.net/omnistack8?retryWrites=true&w=majority',{
+mongoose.connect(db.connection,{
     useUnifiedTopology: true,
     useNewUrlParser: true
 });
